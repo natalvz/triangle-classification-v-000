1226686750c3585141526fd3ee9dc1aa799415ac
Natalia Castro <nat.alvz@gmail.com>
2018-06-10 02:52:45 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -6,7 +6,7 @@ class Triangle
   end
   
   def kind
-    if @length_1.eql?(@length_2) && @length_1.eql?(@length_3) && @length_2.eql?(@length_3)
+    if @length_1 == @length_2 && @length_2 == @length_3 
       :equilateral
     else @length_1 == @length_2 && @length_2 != @length_3 
       :isosceles
