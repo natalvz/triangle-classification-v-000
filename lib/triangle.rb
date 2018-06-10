b14349e6ddd47cd67ed90ae774d1f6c918d9626b
Natalia Castro <nat.alvz@gmail.com>
2018-06-10 03:11:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,12 +9,13 @@ class Triangle
   
   def false_triangl
     if @length_1 == 0 || @length_2 == 0 || @length_3 == 0
+        true
       elsif @length_1 + @length_2 > @length_3
-        raise TriangleError
+
       elsif @length_2 + @length_3 > @length_1
-        raise TriangleError
+
       elsif @length_1 + @length_3 > @length_2
-        raise TriangleError
+
     
   def kind
     if @length_1 == 0 || @length_2 == 0 || @length_3 == 0
