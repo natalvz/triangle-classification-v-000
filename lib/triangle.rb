744a23151751cd06744a73edb7b7aaf20c970497
Natalia Castro <nat.alvz@gmail.com>
2018-06-10 02:54:08 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,6 +10,12 @@ class Triangle
       :equilateral
     elsif @length_1 == @length_2 && @length_2 != @length_3 
       :isosceles
+    elsif @length_1 != @second_side && @second_side == @third_side
++      :isosceles
++    elsif @first_side == @third_side && @first_side != @second_side
++      :isosceles
++    elsif @first_side != @second_side && @second_side != @third_side && @first_side != @third_side
++      :scalene
     end
     
   end
