d63aaa4faec8f6ea588aa7eca708cbbcea7ecbb4
Natalia Castro <nat.alvz@gmail.com>
2018-06-10 02:57:28 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -19,6 +19,8 @@ class Triangle
     elsif @length_1 != @length_2 && @length_2 != @length_3 && @length_1 != @length_3
       :scalene
     end
-    
+  end
+  
+  class TriangleError < StandardError
   end
 end
