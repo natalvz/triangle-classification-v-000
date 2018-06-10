ffce85731597e6f91c48f2efc1499297816de398
Natalia Castro <nat.alvz@gmail.com>
2018-06-10 02:55:57 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -2,7 +2,9 @@ class Triangle
   
   attr_reader :length_1, :length_2, :length_3, :equilateral, :isosceles, :scalene
   def initialize(length_1, length_2, length_3)
-    
+    @length_1 = length_1
+    @length_2 = length_2
+    @length_3 = length_3
   end
   
   def kind
